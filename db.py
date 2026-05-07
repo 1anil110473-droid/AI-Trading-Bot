acadbabf9ca87c0c9f5a261e583a72a61a6475cb
1anil110473-droid <1anil110473@gmail.com>
2026-05-07 14:15:40 +0530

    db.py

---
+++ db.py
@@ -0,0 +1,74 @@
+from sqlalchemy import create_engine,text
+import os
+
+engine=create_engine(os.getenv("DATABASE_URL"))
+
+def init_db():
+
+    with engine.begin() as conn:
+
+        conn.execute(text("""
+
+        CREATE TABLE IF NOT EXISTS trades(
+
+            id SERIAL PRIMARY KEY,
+            symbol TEXT,
+            action TEXT,
+            price FLOAT,
+            qty INT,
+            pnl FLOAT,
+            reason TEXT,
+            time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+
+        )
+
+        """))
+
+        conn.execute(text("""
+
+        CREATE TABLE IF NOT EXISTS ai_weights(
+
+            indicator TEXT PRIMARY KEY,
+            weight FLOAT
+
+        )
+
+        """))
+
+        conn.execute(text("""
+
+        CREATE TABLE IF NOT EXISTS analytics(
+
+            id SERIAL PRIMARY KEY,
+            win_rate FLOAT,
+            total_profit FLOAT,
+            trades INT
+
+        )
+
+        """))
+
+def save_trade(symbol,action,price,qty,pnl,reason):
+
+    with engine.begin() as conn:
+
+        conn.execute(text("""
+
+        INSERT INTO trades(
+        symbol,action,price,qty,pnl,reason
+        )
+
+        VALUES(
+        :s,:a,:p,:q,:pn,:r
+        )
+
+        """),{
+
+            "s":symbol,
+            "a":action,
+            "p":price,
+            "q":qty,
+            "pn":pnl,
+            "r":reason
+
+        })
