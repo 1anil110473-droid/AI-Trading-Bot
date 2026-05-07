399ed61fb61b3ec4bbd7fdc9591f03edcc164588
1anil110473-droid <1anil110473@gmail.com>
2026-05-07 14:20:44 +0530

    telegram_control.py

---
+++ telegram_control.py
@@ -0,0 +1,14 @@
+import requests
+import os
+
+TOKEN=os.getenv("TELEGRAM_TOKEN")
+CHAT_ID=os.getenv("CHAT_ID")
+
+def send(msg):
+
+    url=f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+
+    requests.post(url,data={
+        "chat_id":CHAT_ID,
+        "text":msg
+    })
