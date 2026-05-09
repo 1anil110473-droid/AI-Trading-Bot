41923bb86f06c44b26df72adc143a4107d62cc8e
1anil110473-droid <1anil110473@gmail.com>
2026-05-09 16:47:13 +0530

    telegram_control.py

--- telegram_control.py
+++ telegram_control.py
@@ -1,14 +1,38 @@
 import requests
 import os
+import time
 
-TOKEN=os.getenv("TELEGRAM_TOKEN")
-CHAT_ID=os.getenv("CHAT_ID")
+TOKEN = os.getenv("TELEGRAM_TOKEN")
+
+CHAT_ID = os.getenv("CHAT_ID")
 
 def send(msg):
 
-    url=f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
 
-    requests.post(url,data={
-        "chat_id":CHAT_ID,
-        "text":msg
-    })
+    payload = {
+
+        "chat_id": CHAT_ID,
+        "text": msg
+
+    }
+
+    for _ in range(3):
+
+        try:
+
+            r = requests.post(
+                url,
+                data=payload,
+                timeout=15
+            )
+
+            if r.status_code == 200:
+
+                return True
+
+        except:
+
+            time.sleep(2)
+
+    return False
