31385ff3c6bfcc8af9aebb91d53159e3f1e95c9e
1anil110473-droid <1anil110473@gmail.com>
2026-05-14 23:41:33 +0530

    db.py

--- db.py
+++ db.py
@@ -9,8 +9,19 @@ import json
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(
+
     DATABASE_URL,
-    pool_pre_ping=True
+
+    pool_pre_ping=True,
+
+    pool_recycle=300,
+
+    pool_size=5,
+
+    max_overflow=10,
+
+    pool_timeout=30
+
 )
 
 # =========================================================
